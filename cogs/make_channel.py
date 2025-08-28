8e30aba92b4e89c8b666e9bb6297e0042633e945
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-08-28 21:29:05 +0200

    main

--- cogs/make_channel.py
+++ cogs/make_channel.py
@@ -20,7 +20,8 @@ class make_channel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def make_channel_def(self, author: dc.Member, user: dc.Member): 
+    async def make_channel_def(self, author: dc.Member, user: dc.Member, which_way, channel): 
+
 
         overwrites = {
             self.bot.inviteguild.default_role: dc.PermissionOverwrite(view_channel=False),  
@@ -34,9 +35,11 @@ class make_channel(commands.Cog):
         embed_invite = dc.Embed(title = "Joine dem Quizkanal", description = f"{self.bot.invitelink}\n", color = 0x0099E1)  
 
         embed_create_quiz = dc.Embed(title = "Erstellt ein Quiz", description = f"** {author.mention} {user.mention} stellt euer Quiz zusammen und startet es!**\n\n Bitte wählt die Anzahl an Fragen, den Themenbereich \nund die Schwierigkeit des Quizes aus.\n", color = 0x0099E1)
-
-        await author.send(embed =embed_invite)
-        await user.send(embed =embed_invite)
+        if which_way == 0:
+            await author.send(embed =embed_invite)
+            await user.send(embed =embed_invite)
+        else:
+            await channel.send(embed=embed_invite)
 
         view = Quiz_create_View(user, author, self.bot)
         
@@ -45,16 +48,27 @@ class make_channel(commands.Cog):
         while not view.is_finished():
                 await asyncio.sleep(1)
         
-        view_skip = Quiz_skip_View(self.bot)
         database = database_class(self.bot)
         fragen = await database.find_questions()
+        self.bot.choosen_kategories = ""
+        self.bot.choosen_difficulties = ""
+        self.bot.anzahlfragen = 0
+
         if fragen:
-            for frage in fragen:       
-                question_embed = dc.Embed(title=frage[1], description=f"Bitte gebt sendet eure Antworten ein.\n Wer die Frage als erstes beantwortet hat gewonnen", color=0x0099E1)
-                view_skip = Quiz_skip_View(self.bot)
+            for frage in fragen:   
+                global string_dificulty 
+                string_dificulty = "" 
+                if frage[3] == "EASY":
+                    string_dificulty = "Einfach"
+                if frage[3] == "MEDIUM":
+                    string_dificulty = "Normal"
+                if frage[3] == "HARD":
+                    string_dificulty = "Schwer"  
+                question_embed = dc.Embed(title=frage[1], description=f"Schwierigkeit: {string_dificulty} \n Kategorie: {frage[4]}\n\nBitte gebt sendet eure Antworten ein.\n", color=0x0099E1)
+                view_skip = Quiz_skip_View(self.bot, frage, user, author)
                 await self.bot.invite_channel.send(embed=question_embed, view=view_skip) 
                 
-                check_question_cog = check_questions_class(self.bot, frage[2], self.bot.invite_channel)
+                check_question_cog = check_questions_class(self.bot, frage[2], self.bot.invite_channel, user, author)
                 self.bot.add_cog(check_question_cog)
 
                 self.bot.send_message_allowed = True
@@ -68,11 +82,15 @@ class make_channel(commands.Cog):
 
                 await asyncio.sleep(2)
                 continue
+            view_end = Quiz_restart_View(self.bot, user, author)
+            embed_end = dc.Embed(title="Quiz beendet", description=f"Das Quiz ist nun beendet. Vielen Dank fürs Mitmachen {author.mention} und {user.mention}!\n\nIhr könnt den Kanal nun verlassen oder ein neues Quiz starten.\nDieser Kanal wird in 2 Minuten gelöscht", color=0x0099E1)
+            await self.bot.invite_channel.send(embed=embed_end, view = view_end)
 
 
         else:
+            view_end = Quiz_restart_View(self.bot, user, author)
             embed_1 = dc.Embed(title="Keine Fragen gefunden", description="Es wurden keine Fragen gefunden, die den ausgewählten Kriterien entsprechen. Bitte startet das Quiz erneut und wählt andere Kriterien aus.\n", color=0xF93A2F)
-            await self.bot.invite_channel.send(embed=embed_1)
+            await self.bot.invite_channel.send(embed=embed_1, view = view_end)
 
 async def create_quiz_channel(self, guild : dc.Guild, user : dc.Member, author : dc.Member, overwrites = None):
 
@@ -81,15 +99,42 @@ async def create_quiz_channel(self, guild : dc.Guild, user : dc.Member, author :
     invite_channel = channel
     self.bot.invite_channel = invite_channel
 
+class Quiz_restart_View(dc.ui.View):
+    def __init__(self, bot, user : dc.Member, author : dc.Member):
+        super().__init__(timeout=120)
+        self.bot = bot
+        self.user = user
+        self.author = author
+    
+
+    async def on_timeout(self):
+        self.disable_all_items()
+        await self.message.edit(view=self)
+        await self.bot.invite_channel.delete()
+        self.stop()
+
+    @dc.ui.button(label="Neues Quiz starten", style=dc.ButtonStyle.green)
+    async def button_restart_quiz_callback(self, button, interaction : dc.Interaction):
+        self.bot.send_message_allowed = False
+        await make_channel.make_channel_def(self, self.author, self.user, 1, self.bot.invite_channel)
+        self.disable_all_items()
+        await interaction.message.edit(view=self)
+        await interaction.response.defer()
+        self.stop()
+
+
 class Quiz_skip_View(dc.ui.View):
-    def __init__(self, bot):
+    def __init__(self, bot, frage, user : dc.Member, author : dc.Member):
         super().__init__(timeout=None)
         self.bot = bot
+        self.frage = frage
+        self.user = user
+        self.author = author
 
     @dc.ui.button(label="Frage überspringen", style=dc.ButtonStyle.blurple)
     async def button_skip_question_callback(self, button, interaction):
         self.bot.send_message_allowed = False
-        embed_skip = dc.Embed(title="Frage übersprungen", description="Die Frage wird übersprungen.", color=0xF93A2F)
+        embed_skip = dc.Embed(title="Frage übersprungen", description=f"Die Frage wird übersprungen.\n Die Richtige Antwort lautet: {self.frage[2]}\n\n **Aktueller Punktestand**\n {self.author.mention}:  \n {self.user.mention}:  \n", color=0xF93A2F)
         await interaction.response.send_message(embed=embed_skip)
         self.disable_all_items()
         await interaction.message.edit(view=self)
@@ -162,6 +207,7 @@ class Dropdown_Kategorie(dc.ui.Select):
             string = string[:-2] 
             string_of_kategories = "".join(list_of_kategories)
             string_of_kategories = string_of_kategories[:-2]
+            self.bot.string_of_kategories = string
             self.bot.choosen_kategories = string_of_kategories
         await interaction.response.defer() 
        # embed_callback = dc.Embed(title="Themenfeld ausgewählt", description=f"{self.user.mention} hat {string} als Themenfeld ausgewählt.\n {self.author.mention}", color=0x0099E1)
@@ -204,6 +250,7 @@ class Dropdown_Schwierigkeit(dc.ui.Select):
         string1 = string1[:-2]
         string_of_difficulties = "".join(list_of_difficulties)
         string_of_difficulties = string_of_difficulties[:-2]
+        self.bot.string_of_difficulties = string1
         self.bot.choosen_difficulties = string_of_difficulties
         await interaction.response.defer() 
         #await interaction.response.send_message(f"{self.user.mention} hat {string1} als Schwierigkeit ausgewählt.\n {self.author.mention}")
@@ -257,9 +304,9 @@ class Quiz_create_View(dc.ui.View):
         _disabled = True
         if self.bot.anzahlfragen != 0 and self.bot.choosen_kategories != "" and self.bot.choosen_difficulties != "":
             self.disable_all_items()
-            newembed = dc.Embed(title = "Erstelle ein Quiz", description = f"{self.author.mention} und {self.user.mention} stellt euer Quiz zusammen und startet es!\n\n**Schwierigkeit**\n Ihr habt {self.bot.choosen_difficulties} als Schwierigkeit ausgewählt. \n\n **Themenbereich**\n Ihr habt {self.bot.choosen_kategories} als Themenbereich ausgewählt.\n\n **Fragenanzahl** \n Ihr habt {self.bot.anzahlfragen} als Fragenanzahl ausgewählt\n", color = 0x00D166)
+            newembed = dc.Embed(title = "Erstelle ein Quiz", description = f"{self.author.mention} und {self.user.mention} stellt euer Quiz zusammen und startet es!\n\n**Schwierigkeit**\n Ihr habt {self.bot.string_of_difficulties} als Schwierigkeit ausgewählt. \n\n **Themenbereich**\n Ihr habt {self.bot.string_of_kategories} als Themenbereich ausgewählt.\n\n **Fragenanzahl** \n Ihr habt {self.bot.anzahlfragen} als Fragenanzahl ausgewählt\n", color = 0x00D166)
             await self.message.edit(embed=newembed ,view=self)
-            embed_start = dc.Embed(title="Quiz gestartet", description=f"Das Quiz wird in 10s mit {self.user.mention} und {self.author.mention} gestartet.\n", color=0x00D166)
+            embed_start = dc.Embed(title="Quiz gestartet", description=f"Das Quiz wird in ein paar Sekunden mit {self.user.mention} und {self.author.mention} gestartet.\n\n **Regeln**\n Wer als erstes auf die gestellte Frage antwortet gewinnt.", color=0x00D166)
             await interaction.response.send_message(embed=embed_start)
             self.stop()
         else:
