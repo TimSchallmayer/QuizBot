99e38130f0ccc35a79317c681bb2707e17c5afd1
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-08-28 21:29:21 +0200

    main

--- cogs/check_questions.py
+++ cogs/check_questions.py
@@ -2,10 +2,12 @@ import discord as dc
 from discord.ext import commands
 
 class check_questions(commands.Cog):
-    def __init__(self, bot, answer, channel):
+    def __init__(self, bot, answer, channel, user: dc.Member, author: dc.Member):
         self.bot = bot
         self.answer = answer
         self.channel = channel
+        self.user = user
+        self.author = author
 
     @commands.Cog.listener()
     async def on_message(self, msg):
@@ -20,10 +22,10 @@ class check_questions(commands.Cog):
             await self.check_answer(msg, msg.author, self.answer)
 
     async def check_answer(self, msg, user: dc.Member, answer1: str):
-        if msg.content.lower() == answer1.lower():
+        if answer1.lower() in msg.content.lower():
             self.bot.send_message_allowed = False
             await msg.add_reaction("✅")
-            embed_correct_answer = dc.Embed(title="✅ Frage beantwortet ✅", description=f"Glückwunsch {user.mention}, deine Antwort ist richtig!\n\nDie richtige Antwort lautet: **{answer1}**", color=0x00D166)
+            embed_correct_answer = dc.Embed(title="✅ Frage beantwortet ✅", description=f"Glückwunsch {user.mention}, deine Antwort ist richtig!\n\nDie richtige Antwort lautet: **{answer1}**\n\n **Aktueller Punktestand**\n {self.author.mention}:  \n {self.user.mention}:  \n", color=0x00D166)
             await msg.channel.send(embed=embed_correct_answer)
         else:
             self.bot.send_message_allowed = True
