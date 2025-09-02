1032b66e6edc783ea7a88ffaec27f05773b39218
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-09-02 14:49:32 +0200

    main

--- cogs/database_punkte.py
+++ cogs/database_punkte.py
@@ -1,4 +1,4 @@
-import discord 
+import discord  as dc
 from discord.ext import commands
 import mysql.connector
 
@@ -18,22 +18,41 @@ class Punkte(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    async def update_user():
-        sql_query = f""" """
+    async def update_user(self, user : dc.Member, punkte: int, wins : int, losses : int, draws : int):
+        sql_query = f""" UPDATE Punkte_system SET punkte = punkte + {punkte}, wins = wins + {wins}, losses = losses + {losses}, draws = draws + {draws} WHERE user_id = {user.id} """
+        cursor.execute(sql_query)
+        db.commit()
     
-    async def get_user_Information():
-        sql_query = f""" """
+    async def get_user_Information(self, user : dc.Member):
+        sql_query = f""" SELECT * FROM Punkte_system WHERE user_id = {user.id}"""
+        cursor.execute(sql_query)
+        result = cursor.fetchall()
+        return result
 
-    async def add_user():
-        sql_query = f""" """
+    async def add_user(self, user : dc.Member, punkte: int, wins : int = 0, losses : int = 0, draws : int = 0):
+        sql_query = f""" INSERT INTO Punkte_system (user_id, punkte, wins, losses, draws) VALUES ({user.id}, {punkte}, {wins}, {losses}, {draws}) """
+        cursor.execute(sql_query)
+        db.commit()
     
-    async def remove_user():
-        sql_query = f""" """
+    async def remove_user(self, user : dc.Member):
+        sql_query = f""" DELETE FROM Punkte_system WHERE user_id = {user.id} """
+        cursor.execute(sql_query)
+        db.commit()
     
-    async def get_leaderboard():
-        sql_query = f""" """
+    async def get_leaderboard(self, range: int):
+        sql_query = f"""SELECT * FROM Punkte_system ORDER BY punkte DESC LIMIT {range}"""
+        cursor.execute(sql_query)
+        result = cursor.fetchall()
+        return result
     
-
+    async def does_user_exist(self, user : dc.Member):
+        sql_query = f""" SELECT * FROM Punkte_system WHERE user_id = {user.id} """
+        cursor.execute(sql_query)
+        result = cursor.fetchall()
+        if len(result) == 0:
+            return False
+        else:
+            return True
 
 async def setup(bot):
     await bot.add_cog(Punkte(bot))
